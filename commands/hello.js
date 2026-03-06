8cd3d79eec18c1e1da1d0f70e3d7dd64862991a7
patYczakus <75316297+Patyczakus@users.noreply.github.com>
2026-03-06 20:28:29 +0100

    Wywalenie syfu, który nie działa

--- commands/hello.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'hello',
-  description: 'Bot się przywituje',
-  execute(message, args) {
-    message.reply(`👋 Cześć ${message.author.username}! Jak się masz?`);
-  },
-};
